4644a6201fc318ce8912e81ad4e26034664903f9
Nikita  Pravorov <pravorovnikita@mac.home>
2025-11-18 20:31:50 +0100

    feat: use generated firebase config

---
+++ firebase.config.template.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "__VITE_FIREBASE_API_KEY__",
+  authDomain: "__VITE_FIREBASE_AUTH_DOMAIN__",
+  projectId: "__VITE_FIREBASE_PROJECT_ID__",
+  storageBucket: "__VITE_FIREBASE_STORAGE_BUCKET__",
+  messagingSenderId: "__VITE_FIREBASE_MESSAGING_SENDER_ID__",
+  appId: "__VITE_FIREBASE_APP_ID__",
+  measurementId: "__VITE_FIREBASE_MEASUREMENT_ID__"
+};
