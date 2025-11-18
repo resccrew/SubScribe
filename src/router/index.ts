c4bf5360fe521745a0f85a0e801d1f46475a015e
Nikita  Pravorov <pravorovnikita@mac.home>
2025-11-18 20:09:14 +0100

    fix: Use hash history for GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { auth } from '@/config/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
