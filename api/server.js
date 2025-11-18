532377ead8de16f3e2a9e34c28e73b6775d18647
Nikita  Pravorov <pravorovnikita@mac.home>
2025-11-18 19:13:32 +0100

    refactor: Remove dotenv from production server code

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
 
-dotenv.config();
+
+
 
 const app = express();
 const port = process.env.PORT || 3001;
