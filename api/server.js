5eaf2832b4256be0c495ac57b658138e20f348bc
Nikita  Pravorov <pravorovnikita@mac.home>
2025-11-18 15:26:34 +0100

    fix: Remove wildcard route from API server

--- api/server.js
+++ api/server.js
@@ -213,9 +213,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const distPath = path.resolve(__dirname, '../dist')
 app.use(express.static(distPath))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'))
-})
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
